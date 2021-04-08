43eb121f253a246f4c371aec5659be46914ecaa3
Mochamad Arifin <klampok.child@gmail.com>
2021-04-08 15:14:09 +0700

    rename index.ts

--- build.sh
+++ build.sh
@@ -20,9 +20,9 @@ OUT_DIR="."
 PROTO_FILE="whatsapp.proto"
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="${OUT_DIR}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     $PROTO_FILE
 
 mv -f whatsapp_pb.js index.js
-mv -f whatsapp.ts index.d.ts
+mv -f whatsapp.ts index.ts
